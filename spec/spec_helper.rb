6f1cb5c8e35ffa8bb9fc0da6b95872e0ca5a9939
Nikita Pupko <work.pupko@gmail.com>
2021-04-02 18:11:07 +0300

    Add test coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require_relative 'support/coverage'
+
 require 'bundler/setup'
 require 'dude'
 
---
+++ spec/support/coverage.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+if ENV['COVERAGE'] == 'true'
+  require 'simplecov'
+  require 'simplecov-cobertura'
+
+  SimpleCov.formatter = SimpleCov::Formatter::CoberturaFormatter
+
+  SimpleCov.start do
+    add_filter '/spec/'
+    enable_coverage :branch
+  end
+end
